9c4aa1fda74b99ebc322209e514dfa4911a8cf64
mangesh-landge <mangesh.landge@perennialsys.com>
2022-10-20 11:22:45 +0530

    initial

---
+++ src/pages/createAccount.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CreateAccount() {
+  return (
+    <>
+      <h1>Create Acount Screen</h1>
+    </>
+  );
+}
---
+++ src/pages/editProfile.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EditProfile() {
+  return (
+    <>
+      <h1>Edit profile</h1>
+    </>
+  );
+}
